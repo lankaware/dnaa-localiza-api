898a2e47df2c818835044ccfa88616dc6e2de2ec
jvanderlei <joaop.vanderlei@gmail.com>
2022-02-08 19:50:50 -0300

    20220208J

--- controllers/mktevent.js
+++ controllers/mktevent.js
@@ -11,6 +11,7 @@ const ModelName = mongoose.model("MktEvent")
 const routeName = "/mktevent"
 
 module.exports = app => {
+
     app.get(routeName, tokenok, async (req, res) => {
         await ModelName.find()
             .sort('name')
@@ -80,25 +81,36 @@ module.exports = app => {
     })
 
     app.get(routeName + "previous/:project", async (req, res) => {
-        const uniqueLocationsArray = [];
+        const LocationsArray = [];
+        // const uniqueLocationsArray = [];
         const project = mongoose.Types.ObjectId(req.params.project)
         await ModelName.find({ "reproject_id": project }, '_id')
             .then(async (mkteventList) => {
                 for (const el of mkteventList) {
                     await EventLocationModelName.find({ "event_id": el._id })
                         .then((eventLocationList) => {
-                             for (const location of eventLocationList) {
-                                console.log("location", location.distance)
-                                uniqueLocationsArray.push(location)
+                            for (const location of eventLocationList) {
+                                if (!LocationsArray.includes(el.location_id))
+                                    LocationsArray.push(location.location_id.toString())
                             }
                         })
                 }
-                
-                // uniqueLocationsArray = [...uniqueEventLocations]
-                console.log("uniqueLocationsArray", uniqueLocationsArray)
+                // return uniqueLocationsArray = LocationsArray.filter((item, pos) => {
+                //     return LocationsArray.indexOf(item) === pos;
+                // })
+                // console.log("uniqueLocationsArray", uniqueLocationsArray)
+                return LocationsArray
+            }).then((LocationsArray) => {
+                return LocationsArray.filter((item, pos) => {
+                    console.log(mongoose.Types.ObjectId(item))
+                    console.log(LocationsArray.indexOf(item))
+                    return LocationsArray.indexOf(item) === pos;
+                })
+            })
+            .then((record) => {
                 return res.json({
                     error: false,
-                    record: uniqueLocationsArray
+                    record: record
                 })
             })
             .catch((err) => {
@@ -108,6 +120,7 @@ module.exports = app => {
                 })
             })
     })
+
 
     app.post(routeName, tokenok, async (req, res) => {
         await ModelName.create(req.body)
@@ -172,3 +185,70 @@ module.exports = app => {
             })
     })
 }
+
+
+// app.get(routeName + "previous/:project/:event", async (req, res) => {
+//     const LocationsArray = [];
+//     const uniqueLocationsArray = [];
+//     const filteredArray = [];
+//     const project = mongoose.Types.ObjectId(req.params.project)
+//     await ModelName.find({ "reproject_id": project }, '_id')
+//         .then(async (mkteventList) => {
+//             for (const el of mkteventList) {
+//                 console.log("el", el)
+//                 await EventLocationModelName.aggregate([
+//                     {
+//                         $lookup:
+//                         {
+//                             from: 'locations',
+//                             localField: 'location_id',
+//                             foreignField: '_id',
+//                             as: 'location'
+//                         }
+//                     },
+//                     {
+//                         $match: { 'event_id': el._id }
+//                     },
+//                     {
+//                         $project:
+//                         {
+//                             _id: 1,
+//                             event_id: 1,
+//                             location_id: 1,
+//                             location_profile: '$location.profile',
+//                             location_name: '$location.name',
+//                             location_address: '$location.address',
+//                             location_zip: '$location.zip',
+//                             location_city: '$location.city',
+//                             distance: 1,
+//                             disponibility: 1,
+//                             selected: 1,
+//                         }
+//                     },
+//                     {
+//                         $sort: { 'date': 1 },
+//                     }
+//                 ]).then((location) => {
+//                     uniqueLocationsArray.push(location)
+//                 })
+//             }
+//             uniqueLocationsArray.forEach((el) => {
+//                 if(!filteredArray.includes(el.location_id))
+//                     filteredArray.push(el.location_id)
+//             })
+
+
+//             return uniqueLocationsArray.forEach()
+//         }).then((uniqueLocationsArray) => {
+//             return res.json({
+//                 error: false,
+//                 record: uniqueLocationsArray
+//             })
+//         })
+//         .catch((err) => {
+//             return res.json({
+//                 error: true,
+//                 message: err
+//             })
+//         })
+// })
